79172ff5217e6d6fe5f5222bae17c7cca6fa9534
Alex Tikhonov <tau9501@gmail.com>
2022-04-08 21:53:01 +0300

    Added lesson11

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+'use strict';
 
+const e_btn = document.getElementById('e_btn');
+const square = document.getElementById('square');
+const inputText = document.querySelector('input[type=text]');
+const btn = document.getElementById('btn');
+const inputRange = document.querySelector('input[type=range]');
+const span = document.getElementById('range-span');
+const circle = document.getElementById('circle');
 
+btn.onclick = function () {
+    square.style.backgroundColor = inputText.value;
+    console.log(inputText.value);
+};
 
+e_btn.style.display = "none";
 
+const logger = function (event) {
+    span.textContent = event.target.value;
+    circle.style.height = +inputRange.value + '%';
+    circle.style.width = +inputRange.value + '%';
+    console.log(inputRange.value);
+};
 
+inputRange.addEventListener('input', logger);
+inputRange.addEventListener('change', logger);
